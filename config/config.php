8c9c0543e3d2851b5e82dbde3bb3b4f7b57f0dc3
jairocod3000 <jairoalejandro9002@gmail.com>
2026-05-29 18:13:19 +0200

    Arreglo en config.php

--- config/config.php
+++ config/config.php
@@ -1,5 +1,7 @@
 <?php
-$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https' : 'http';
+$isHttps = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off')
+    || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https');
+$protocol = $isHttps ? 'https' : 'http';
 $host = $_SERVER['HTTP_HOST'] ?? 'localhost';
 define('BASE_URL', getenv('BASE_URL') ?: ($protocol . '://' . $host . '/'));
 define('MODO_DESARROLLO', getenv('MODO_DESARROLLO') === 'true');
