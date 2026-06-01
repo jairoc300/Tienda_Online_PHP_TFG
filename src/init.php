19311dd3067ff159472758335020f8ef623258ea
jairocod3000 <jairoalejandro9002@gmail.com>
2026-06-01 19:39:56 +0200

    Arreglo subida de imágenes 2

--- src/init.php
+++ src/init.php
@@ -1,9 +1,9 @@
 <?php
 
 // Mostrar errores para debugging
-ini_set('display_errors', 0);
-ini_set('display_startup_errors', 0);
-error_reporting(0);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(1);
 
 session_start();
 use Routes\Routes;
